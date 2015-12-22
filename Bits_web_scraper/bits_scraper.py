d558159e28598566ae7c45522d54d5c7aa84a408
Nikica Trajkovski <trajkovs@usc.edu>
2015-12-21 22:22:12 -0800

    added scrape for content page

--- Bits_web_scraper/bits_scraper.py
+++ Bits_web_scraper/bits_scraper.py
@@ -11,13 +11,20 @@ def download_files(url, path):
         print "And you get an HTTPError:", e.message
 
     # Retrieve the webpage
-    main = bs4.BeautifulSoup(response.text, "html.parser")
-
+    main = bs4.BeautifulSoup(response.content, "lxml")
     os.chdir(path) # change the directory
+
+    # Get the Contents Table
+    stylesheet = main.find('link', rel = 'stylesheet')
+    tableFormatting = "<div id=\"content\" class=\"center\">"
+    content = main.find('table')
+    with open("_"+path+".html", "wb") as file:
+        file.write(stylesheet.prettify("utf-8") + tableFormatting + content.prettify("utf-8"))
+
     # Get all pdfs
     itemList = main.findAll('a')
     for i in range(len(itemList)):
-        if itemList[i].get('href').endswith('.pdf'):
+        if itemList[i].get('href').endswith(('.pdf', '.zip', '.rar')):
             os.system('wget -q -nd -np -c -N %s'%itemList[i].get('href'))
 
     # Get the HTML from assignments, labs
@@ -26,8 +33,9 @@ def download_files(url, path):
     filenames = [] # Holds the filenames for the download
     for i in range (len(linkList)):
         try:
-            forDL.append(linkList[i].find('a').get('href'))
-            filenames.append(linkList[i].find('a').text)
+            if linkList[i].find('a').get('href').endswith('.html'):
+                forDL.append(linkList[i].find('a').get('href'))
+                filenames.append(linkList[i].find('a').text)
         except AttributeError:
             continue
 
@@ -40,13 +48,14 @@ def download_files(url, path):
             print "And you get an HTTPError:", e.message
 
         # Retrieve the webpage
-        main = bs4.BeautifulSoup(response.text, "html.parser")
+        main = bs4.BeautifulSoup(response.content, "lxml")
+    
         # Get the stylesheet for the page
         stylesheet = main.find('link', rel = 'stylesheet')
         # Get the content for the page
         content = main.find('div', id = 'main')
         print "Downloading ", filenames[i]
-        html = stylesheet.prettify("utf-8") + content.prettify("utf-8")
+        html = stylesheet.prettify("utf-8") + '<meta charset="UTF-8">' + content.prettify("utf-8")
         with open(filenames[i]+".html", "wb") as file:
             file.write(html)
 
@@ -57,13 +66,13 @@ if __name__ == "__main__":
 	res.raise_for_status()
 
 	# Retrieve the webpage
-	main = bs4.BeautifulSoup(res.text, "html.parser")
+	main = bs4.BeautifulSoup(res.text, "lxml")
 
 	# Create a folder with the website title
 	root_dir = main.head.title.text
 	if not os.path.exists(root_dir):
 		os.makedirs(root_dir)
-		os.chdir(root_dir)
+	os.chdir(root_dir)
 
 	print "Downloading from ", main.head.title.text.encode('utf-8')
 	# Look through the list of the main page
@@ -82,3 +91,6 @@ if __name__ == "__main__":
 			url = menu[index].get('href')
 			download_files(url, path)
 			os.chdir('../')
+
+
+# check for encoding when dling html, create a table of content
